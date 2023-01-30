0709685061ea6227154af7a80c00a41b5f804e48
Rafal <kuna240@gmail.com>
2023-01-30 20:01:49 +0000

    clcfinal

--- study-buddy/src/components/molecules/UsersListItem/UsersListItem.js
+++ study-buddy/src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,15 +3,13 @@ import PropTypes from "prop-types";
 import { StyledAverage, StyledInfo, Wrapper } from "./UserListItem.styles";
 import Button from "components/atoms/Button/Button";
 
-const showIndex = (index) => alert(`This is student ${index + 1}`);
-
-const UsersListItem = ({ index, userData: { average, name, attendance } }) => (
+const UsersListItem = ({ deleteUser, userData: { average, name, attendance } }) => (
   <Wrapper>
     <StyledAverage value={average}>{average}</StyledAverage>
     <StyledInfo>
       <p>
         {name}
-        <Button onClick={() => showIndex(index)} />
+        <Button onClick={() => deleteUser(name)} />
       </p>
 
       <p>attendance:{attendance}</p>

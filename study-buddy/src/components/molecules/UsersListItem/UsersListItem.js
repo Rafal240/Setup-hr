83241f9097f883bb9a7e249a4bebd9f915a34d66
Rafal <kuna240@gmail.com>
2023-02-20 19:49:29 +0000

    react context final

--- study-buddy/src/components/molecules/UsersListItem/UsersListItem.js
+++ study-buddy/src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,27 +1,23 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useContext } from "react";
 import { StyledAverage, StyledInfo, Wrapper } from "./UserListItem.styles";
 import Button from "components/atoms/Button/Button";
+import { UsersContext } from "../../../providers/UsersProvider";
 
-const UsersListItem = ({ deleteUser, userData: { average, name, attendance } }) => (
-  <Wrapper>
-    <StyledAverage value={average}>{average}</StyledAverage>
-    <StyledInfo>
-      <p>
-        {name}
-        <Button onClick={() => deleteUser(name)} />
-      </p>
+const UsersListItem = ({ userData: { average, name, attendance } }) => {
+  const { deleteUser } = useContext(UsersContext);
+  return (
+    <Wrapper>
+      <StyledAverage value={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>
+          {name}
+          <Button onClick={() => deleteUser(name)} />
+        </p>
 
-      <p>attendance:{attendance}</p>
-    </StyledInfo>
-  </Wrapper>
-);
-
-UsersListItem.propTypes = {
-  userData: PropTypes.shape({
-    average: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    attendance: PropTypes.string,
-  }),
+        <p>attendance:{attendance}</p>
+      </StyledInfo>
+    </Wrapper>
+  );
 };
+
 export default UsersListItem;
--- study-buddy/src/views/App.js
+++ study-buddy/src/views/App.js
@@ -1,67 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { Wrapper } from "./App.styles";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../assets/styles/GlobalStyle";
 import { theme } from "../assets/styles/theme";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { users as usersData } from "data/users";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUser from "../views/AddUser";
 import { MainTemplate } from "components/templates/MainTemplate";
 import Dashboard from "./Dashboard";
-
-const initialFormState = {
-  name: "",
-  attendance: "",
-  average: "",
-};
+import UsersProvider from "providers/UsersProvider";
 
 const App = () => {
-  const [users, setUsers] = useState(usersData);
-  const [formValues, setFormValues] = useState(initialFormState);
-
-  const deleteUser = (name) => {
-    const filtredUser = users.filter((user) => user.name !== name);
-    setUsers(filtredUser); //w funkcji urzywamy drugiego argumentu z tablicy do któego wkladamy nowy stan w tym przypadku filtred user
-  };
-  const handleInputChange = (e) => {
-    console.log(formValues);
-    setFormValues({
-      ...formValues, // Wrzucamy wszystkie poprzednie wartosci tego formularza
-      [e.target.name]: e.target.value,
-    });
-
-    // a jezeli dochodzi do zmiany wartoci jednego pola to ta zmiana zostanie przypisana do klucza w naszym obiekcie czyt.name attendance average
-  };
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const newUser = {
-      name: formValues.name,
-      attendance: formValues.attendance,
-      average: formValues.average,
-    };
-    setUsers([newUser, ...users]);
-    setFormValues(initialFormState);
-  };
-
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainTemplate>
-          <Wrapper>
-            <nav>
-              <Link to="/"></Link>
-              <Link to="/add-user"></Link>
-            </nav>
-            <Routes>
-              <Route
-                exact
-                path="/add-user"
-                element={<AddUser formValues={formValues} handleAddUser={handleAddUser} handleInputChange={handleInputChange} />}
-              ></Route>
-              <Route exact path="/" element={<Dashboard deleteUser={deleteUser} users={users} />}></Route>
-            </Routes>
-          </Wrapper>
+          <UsersProvider>
+            <Wrapper>
+              <Routes>
+                <Route exact path="/add-user" element={<AddUser />}></Route>
+                <Route exact path="/" element={<Dashboard />}></Route>
+              </Routes>
+            </Wrapper>
+          </UsersProvider>
         </MainTemplate>
       </ThemeProvider>
     </Router>
